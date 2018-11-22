b4c6993b709ec9cba0834e95ecbab7847ba9115b
Sagi Sheinfeld <ssheinfeld@preempt.com>
2018-11-22 14:35:32 +0200

    SYS-12244: make computeResponseNTLMv2 support external AvPairs include all data

--- impacket/ntlm.py
+++ impacket/ntlm.py
@@ -914,10 +914,10 @@ def computeResponseNTLMv2(flags, serverChallenge, clientChallenge, serverName, d
            aTime = struct.pack('<q', (116444736000000000 + calendar.timegm(time.gmtime()) * 10000000) )
            #aTime = '\x00'*8
            av_pairs[NTLMSSP_AV_TIME] = aTime
-        serverName = av_pairs.getData()
     else:
         aTime = av_pairs[NTLMSSP_AV_TIME][1]
-        serverName = av_pairs[NTLMSSP_AV_HOSTNAME][1]
+    serverName = av_pairs.getData()
+          
     temp = responseServerVersion + hiResponseServerVersion + b'\x00' * 6 + aTime + clientChallenge + b'\x00' * 4 + \
            serverName + b'\x00' * 4
 
